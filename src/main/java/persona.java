44252ea406ab352e6936e6c596e8b3fb73315208
Marcogit <mcrodriguezc226@gmail.com>
2026-02-10 09:23:41 +0100

    clase-persona

---
+++ src/main/java/persona.java
@@ -0,0 +1,10 @@
+public class persona {
+    static void main() {
+         class perosona{
+            private String name;
+            private String surname;
+            private  int edad;
+        }
+    }
+
+}
